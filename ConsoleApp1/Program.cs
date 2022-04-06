b1e092b3f54ef500860c47545d1ca8ed406fb131
Олег Коновалов <konovalov.105s1@stud.asu.ru>
2022-04-06 20:55:30 +0700

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Net;
+using System.Text.RegularExpressions;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System;
+
+class Programm
+{
+    static void Main()
+    {
+        Console.Write("x = ");
+        double x = double.Parse(Console.ReadLine());
+        double y, j;
+        try
+        {
+            y = Math.Sqrt(x + 1);
+            j = y;
+        }
+        catch
+        {
+            Console.WriteLine("Невозможно извлечь квадратный корень из данного числа.");
+            return;
+        }
+        try
+        {
+            y = Math.Cbrt(9 - y);
+        }
+        catch
+        { 
+            Console.WriteLine("Невозможно извлечь кубический корень из данного выражения.");
+            return;
+        }
+        try
+        {
+            y += Math.Cbrt(7 + j);
+            Console.WriteLine(y);
+            if (y == 4) Console.WriteLine("Выражение равно 4-ём.");
+            else Console.WriteLine("Выражение не равно 4-ём.");
+        }
+        catch
+        {
+            Console.WriteLine("Невозможно извлечь кубический корень из данного выражения.");
+        }
+    }
+}
